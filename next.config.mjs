268053c317aecdc1b17d8eb08ae9e46e6491e26e
James Fisher <dwfishman4@gmail.com>
2026-04-06 19:39:16 -0700

    Added redirect for old links

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: '/dev',
+        destination: '/',
+        permanent: true,
+      },
+      {
+        source: '/dev/:path*',
+        destination: '/:path*',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
